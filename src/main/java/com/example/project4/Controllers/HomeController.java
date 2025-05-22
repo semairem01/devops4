b39301680529f987ed7351e82f2afc7a2069c4bc
semairem01 <semairem026@hotmail.com>
2025-05-22 09:27:39 +0300

    Controller changed.

--- src/main/java/com/example/project4/Controllers/HomeController.java
+++ src/main/java/com/example/project4/Controllers/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String getHome(){
-        return "Hellooo from Kubernates Cluster!";
+        return "Hellooooooooooo from Kubernates Cluster!";
     }
 }
